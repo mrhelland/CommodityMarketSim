7c457622cde932385142a6793b59fa67bb3f1bd2
Tim Helland <tim.helland@lebanon.k12.or.us>
2024-10-13 10:26:29 -0700

    Create a browser for default and user created commodity images.

---
+++ CommodityMarketSim/CommodityIconBrowser.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CommodityMarketSim {
+    partial class CommodityIconBrowser {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.lvImages = new System.Windows.Forms.ListView();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvImages
+            // 
+            this.lvImages.HideSelection = false;
+            this.lvImages.LargeImageList = this.imgList;
+            this.lvImages.Location = new System.Drawing.Point(13, 13);
+            this.lvImages.Name = "lvImages";
+            this.lvImages.Size = new System.Drawing.Size(853, 371);
+            this.lvImages.SmallImageList = this.imgList;
+            this.lvImages.TabIndex = 0;
+            this.lvImages.UseCompatibleStateImageBehavior = false;
+            // 
+            // imgList
+            // 
+            this.imgList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imgList.ImageSize = new System.Drawing.Size(64, 64);
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.Controls.Add(this.btnImport, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSelect, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnCancel, 3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 390);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(853, 48);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImport.Location = new System.Drawing.Point(3, 3);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(144, 42);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import...";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelect.Location = new System.Drawing.Point(556, 3);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(144, 42);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(706, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(144, 42);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // CommodityIconBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(878, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.lvImages);
+            this.Name = "CommodityIconBrowser";
+            this.Text = "Commodity Icons";
+            this.Load += new System.EventHandler(this.CommodityIconBrowser_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvImages;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ CommodityMarketSim/CommodityIconBrowser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MarketFrameworkLibrary;
+
+namespace CommodityMarketSim {
+    public partial class CommodityIconBrowser : Form {
+
+        private Image selectedImage;
+        private string selectedImageName;
+
+        public Image SelectedImage {
+            get => selectedImage;
+            set => selectedImage = value;
+        }
+
+        public CommodityIconBrowser() {
+            InitializeComponent();
+        }
+
+        private void CommodityIconBrowser_Load(object sender, EventArgs e) {
+            populateImageResources();
+        }
+
+        private void populateImageResources() {
+            imgList.Images.Clear();
+            SortedList<string, Image> images = Commodity.Images;
+            for(int i = 0; i < images.Count; i++) {
+                imgList.Images.Add(images.Values[i]);
+                ListViewItem item = new ListViewItem();
+                item.Text = images.Keys[i];
+                item.ImageIndex = i;
+                lvImages.Items.Add(item);
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = "png";
+            ofd.Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if(ofd.ShowDialog() == DialogResult.OK) {
+                FileInfo f = new FileInfo(ofd.FileName);
+                string name = f.Name.Replace(f.Extension, "");
+                Image newImage = new Bitmap(f.FullName);
+                CropImageForm crop = new CropImageForm();
+                crop.Image = newImage;
+                crop.ShowDialog();
+
+                //string fileSavePath = Path.Combine(Commodity.ImageDirectory, 
+                //newImage.Save()
+
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            selectedImage = null;
+            this.Close();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
